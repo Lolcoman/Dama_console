4d9393a11d2a35c3be95c0d90256b0939ac0ad59
Lolcoman <Lolcoman@users.noreply.github.com>
2020-11-08 22:21:12 +0100

    Update pohybu
    Přidano UI pro pohyb (A2-B3)

--- UI.cs
+++ UI.cs
@@ -18,26 +18,52 @@ namespace Damakonzole
         {
             while (true)
             {
-                Move();
+                Moving();
                 PrintBoard();
             }
 
         }
-
-        public void Move()
+        public void Moving()
         {
-            Console.WriteLine("Zadej souradnice pro pohyb kamene X:");
-            int posX = int.Parse(Console.ReadLine());
-            Console.WriteLine("Zadej souradnice pro pohyb kamene Y:");
-            int posY = int.Parse(Console.ReadLine());
-            Console.WriteLine("Kámen X:{0} Y:{1}",posX,posY);
-            Console.WriteLine("Zadej souradnice kam se chcete pohnout:");
-            int posX1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Zadej souradnice kam se chcete pohnout:");
-            int posY1 = int.Parse(Console.ReadLine());
-            gameDriver.SetValueOnBoard(posX1, posY1, board.Coords(posX,posY));
-            gameDriver.SetValueOnBoard(posX, posY, 0);
-            //board.Coords(posX, posY);
+            Console.Write("Zadej pohyb ve formátu (A2-B3): ");
+            int[] move = new int[] { -1 };
+            while (move[0] < 0)
+            {
+
+                move = InputUser(); // X1,Y1,X2,Y2
+                while (move[0] < 0 || move[0] >= 8 || move.Length > 4)
+                {
+                    Console.Write("I ty prase, zadej ty souřadnice jako člověk: ");
+                    move = InputUser();
+                }
+                int x1 = move[0];
+                int y1 = move[1];
+                int x2 = move[2];
+                int y2 = move[3];
+                gameDriver.SetValueOnBoard(x2, y2, board.Coords(x1, y1));
+                gameDriver.SetValueOnBoard(x1, y1, 0);
+
+            }
+        }
+        public int[] InputUser()
+        {
+            //Vstup uživatele s převedením na malá písmena
+            string input = Console.ReadLine().ToLower();
+            if (input.Length != 5)
+            {
+                return new int[] { -1 };
+            }
+
+            char x1, y1, x2, y2; // X1Y1 vybrany kamen, X2Y2 kam pohnout
+            x1 = input[0];
+            int X1 = (int)(x1 - 'a'); //převod v tabulce ASCII
+            y1 = input[1];
+            int Y1 = (int)(y1 - '1'); //1, protože 0 není v herní desce
+            x2 = input[3];
+            int X2 = (int)(x2 - 'a');
+            y2 = input[4];
+            int Y2 = (int)(y2 - '1');
+            return new int[] { X1, Y1, X2, Y2 };
         }
         /// <summary>
         /// Vykreslení desky
